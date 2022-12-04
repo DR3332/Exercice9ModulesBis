565fd968839c4f727fff8b0d2120d39be88bd9aa
Romain Dussaussois <romain.rd21@gmail.com>
2022-12-04 07:38:28 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice9ModulesBis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice9ModulesBis
+{
+    class Program
+    {
+        static void abs(int n)
+        {
+            
+            if (n < 0)
+            {
+                 Console.WriteLine("Voici la valeur absolu de " + n + " : " + -n);
+            }
+            else
+            {
+                Console.WriteLine("Voici la valeur absolu de " + n + " : " + n);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /* Le programme doit permettre de saisir un entier et d’afficher sa valeur absolue en utilisant la fonction
+            ci-dessus. La fonction précédente doit être transformée en procédure et le programme doit exploiter la procédure
+            (tout en faisant la même chose). */
+
+            Console.WriteLine("Veuillez saisir une valeur : ");
+
+            abs(int.Parse(Console.ReadLine()));
+            Console.ReadLine();
+
+        }
+    }
+}
